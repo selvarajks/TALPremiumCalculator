ab17f11fcd1931d4a3366aa56662e407523a9056
selvarajks <selvarajks@gmail.com>
2021-04-04 16:22:02 +1000

    calculate age based on calendar selection

--- src/components/Premium/PremiumView.tsx
+++ src/components/Premium/PremiumView.tsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import "./style.scss";
+import Datepicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface Props {
   productsLength: number;
+}
+interface state {
+  startDate: Date;
+  age: number;
 }
 
 interface PropsFromDispatch {
@@ -10,39 +16,83 @@ interface PropsFromDispatch {
 }
 
 type AllProps = PropsFromDispatch;
-class PremiumView extends React.Component<AllProps> {
+
+const calculate_age = (dob) => {
+  const birthDate = new Date(dob);
+  const difference = Date.now() - birthDate.getTime();
+  const age = new Date(difference);
+
+  return Math.abs(age.getUTCFullYear() - 1970);
+};
+
+class PremiumView extends React.Component<AllProps, state> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      startDate: new Date(),
+      age: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date,
+      age: calculate_age(date),
+    });
+
+    //this.state.age = calculate_age(date);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
   public render() {
     return (
       <div className="form">
         <table>
-          <tr>
-            <td>
-              <h1>TAL Premium Calculator</h1>
-            </td>
-          </tr>
-          <tr>
-            <label>Name </label>
-            <input name="fullname"></input>
-          </tr>
-          <tr>
-            <label>Age </label>
-            <input name="age"></input>
-          </tr>
-          <tr>
-            <label>DOB </label>
-            <input name="dob"></input>
-          </tr>
-          <tr>
-            <label>Occupation </label>
-            <input name="occupation"></input>
-          </tr>
+          <tbody>
+            <tr>
+              <td>
+                <h1>TAL Premium Calculator</h1>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Name </label>
+                <input name="fullname"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Age </label>
+                <input name="age" value={this.state.age}></input>
+              </td>
+            </tr>
+            <tr>
+              <label>DOB </label>
+              <Datepicker
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                name="startDate"
+                dateFormat="MM/dd/yyyy"
+              />
+            </tr>
+            <tr>
+              <label>Occupation </label>
+              <input name="occupation"></input>
+            </tr>
 
-          <tr>
-            <label>SumInsured </label>
-            <input name="suminsured"></input>
-          </tr>
+            <tr>
+              <label>SumInsured </label>
+              <input name="suminsured"></input>
+            </tr>
+          </tbody>
         </table>
-        <button onClick={this.props.onSubmitClick}>Submit</button>
+        <button onClick={this.onFormSubmit}>Submit</button>
       </div>
     );
   }
